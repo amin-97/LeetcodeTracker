19f9dcb830f9b195cf9d0e8f7dae34b3f517a22e
amin-97 <amin.khan2897@gmail.com>
2024-12-15 09:51:13 -0500

    init commit

---
+++ backend/src/main/java/com/leetcodetracker/backend/LeetCodeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.leetcodetracker.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetCodeTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leetcodetracker/backend/LeetCodeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcodetracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
